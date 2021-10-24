27a89742c3f57e5609015e69540748a3b736d9d4
Syrym Tussupbekov <justsyrym@gmail.com>
2021-10-24 19:24:13 +0300

    implement Huffman decoding

--- src/main/java/me/syrym/greedy/huffman/HuffmanDecoding.java
+++ src/main/java/me/syrym/greedy/huffman/HuffmanDecoding.java
@@ -1,16 +1,13 @@
-/*
 package me.syrym.greedy.huffman;
 
-import me.syrym.FastScanner;
-import me.syrym.greedy.Pair;
-import me.syrym.greedy.huffman.structure.Leaf;
-import me.syrym.greedy.huffman.structure.Node;
-import me.syrym.greedy.huffman.structure.Parent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class HuffmanDecoding extends FastScanner {
+public class HuffmanDecoding {
     private String code;
 
     public static void main(String[] args) {
@@ -22,30 +19,60 @@ public class HuffmanDecoding extends FastScanner {
         int k = fastScanner.nextInt();
         int l = fastScanner.nextInt();
 
-        List<Leaf> leaves = new ArrayList<>();
+        Map<String, String> frequencyMap = new HashMap<>();
         for (int i = 0; i < k; i++) {
             String line = fastScanner.readLine();
-            leaves.add(new Leaf(line.charAt(0), line.substring(2)));
+            frequencyMap.put(
+                    line.substring(3),
+                    Character.toString(line.charAt(0))
+            );
         }
-         this.code = fastScanner.readLine();
-
-        Node prev = leaves.get(leaves.size() - 1);
-        for (int i = leaves.size() - 2; i >= 0; --i) {
-            prev = new Parent(leaves.get(i), prev);
-        }
-
+        String line = fastScanner.readLine();
         StringBuilder sb = new StringBuilder();
-        int i = 0;
-        while (i < code.length()) {
-            Pair<Character, Integer> res = traverse(prev, i);
-            sb.append(res.left);
-            i = res.right;
+        for (int i = 0; i < line.length(); i++) {
+            String curr = "";
+            curr += line.charAt(i);
+            while (!frequencyMap.containsKey(curr) && i+1 < line.length()) {
+                curr += line.charAt(++i);
+            }
+            sb.append(frequencyMap.get(curr));
         }
         System.out.println(sb);
     }
 
-    private Pair<Character, Integer> traverse(Parent node, String curr) {
-        
+    private static class FastScanner {
+        private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private StringTokenizer st = new StringTokenizer("");
+
+        public String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    System.out.println("IO ERROR: " + e.getMessage());
+                }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public String readLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "ERROR!";
+            }
+        }
     }
 }
-*/
